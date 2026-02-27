76144e9c3dff9ab9f7f4475fb0602d56f17e97e8
Yuran Saraiva <yrnsaraiva03@gmail.com>
2026-02-27 16:00:08 +0200

    update static_url

--- runwithbroto/urls.py
+++ runwithbroto/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path("events/", include("events.urls")),
     path("payments/", include("payments.urls")),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
